6516990a970be968ff61a5dc8cd66b67eaf4a9cd
fzyzcjy <ch271828n@outlook.com>
2025-07-23 20:09:13 +0800

    more

--- frontend/src/app/sample/page.tsx
+++ frontend/src/app/sample/page.tsx
@@ -3,7 +3,7 @@ export default function HomePage() {
     <div className="min-h-screen flex flex-col items-center justify-start bg-white py-16 px-4">
       <div className="w-full max-w-5xl">
         <div className="mb-12 flex items-center justify-between">
-          <h1 className="text-3xl font-semibold tracking-tight text-gray-900 w-full text-center">Homepage</h1>
+          <h1 className="text-3xl font-semibold tracking-tight text-gray-900 w-full text-center">Sample</h1>
         </div>
         <p>Hi</p>
       </div>
