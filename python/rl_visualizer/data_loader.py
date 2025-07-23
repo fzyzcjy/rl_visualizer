9edc90787eaaff274af169c73743fd25362ad3a7
fzyzcjy <ch271828n@outlook.com>
2025-07-23 16:59:29 +0800

    more

--- python/rl_visualizer/data_loader.py
+++ python/rl_visualizer/data_loader.py
@@ -1,8 +1,14 @@
+import pickle
 from pathlib import Path
 
 
 def read_dir(dir_base):
     dir_base = Path(dir_base)
 
+    dir_base / "rollout_data"
+    dir_base / "train_data"
 
     return TODO
+
+def _read_folder(directory: Path):
+    return [pickle.load(p) for p in sorted(list(directory.glob("*.pt")))]
