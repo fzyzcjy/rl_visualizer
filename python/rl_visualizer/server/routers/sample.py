fb6a3e4a6e8a8a95f9169e9335c3cb0d0421ae89
fzyzcjy <ch271828n@outlook.com>
2025-07-23 18:22:29 +0800

    more

--- python/rl_visualizer/server/routers/sample.py
+++ python/rl_visualizer/server/routers/sample.py
@@ -1,11 +1,15 @@
 from fastapi import APIRouter, Depends
 from ..data_source import DataSource, get_data_source
+from pydantic import BaseModel
 
 router = APIRouter(
     prefix="/api/sample",
     tags=["sample"],
 )
 
+class SampleGetResponse(BaseModel):
+    pass
+
 @router.get("/{sample_index}")
-def get(sample_index: int, data_source: DataSource = Depends(get_data_source)):
+def get(sample_index: int, data_source: DataSource = Depends(get_data_source)) -> SampleGetResponse:
     return {"value": "sample"}
